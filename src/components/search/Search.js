9e4df9f396b87e691c7720e90bcf3b02ac2b8bde
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-04-26 16:11:21 +0300

    Code refactoring.

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,83 +5,101 @@ import styles from './Search.module.css';
 
 const Search = () => {
     const [search, setSearch] = useState('');
-    const [selected, setSelected] = useState({ list: [], style: { display: 'none' } });
+    const [selected, setSelected] = useState({ list: [], display: false });
+    const [result, setResult] = useState([])
 
     useEffect(() => {
         if (search.length > 0) {
             service.getSuggested(search)
-                .then(res => setSelected({ list: res, style: { display: 'inline' } }))
+                .then(res => setSelected({ list: res, display: true }))
             //.catch(err => console.log(err))
         }
     }, [search])
 
     const onChange = (ev) => {
         setSearch(ev.target.value);
-        setSelected({ list: [], style: { display: 'none' } });
+        setSelected({ list: [], display: false });
     };
 
     const onClose = () => {
         setSearch('');
-        setSelected({ list: [], style: { display: 'none' } });
+        setSelected({ list: [], display: false });
     };
 
     const onFocus = () => {
         setSelected(state => ({
-            ...state, 
-            style: { display: 'block' }
+            ...state,
+            display: true
         }));
     };
 
-    const onFocusBlur = () => {
+    const onSelect = (ev) => {
+        if (ev.target.textContent.length === 0) {
+            return;
+        }
+
+        service.getAddress(ev.target.textContent)
+            .then(res => setResult(res))
+        // .catch(err => console.log(err));
+
         setSelected(state => ({
-            ...state, 
-            style: { display: 'none' }
+            ...state,
+            display: false
         }));
-    };
 
-    console.log(search)
+    }
 
     return (
-        <form className={styles.search}>
-            <div className={styles['search-container']}>
-                <input
-                    type="text"
-                    className={styles['search-container-input']}
-                    name="search"
-                    onChange={onChange}
-                    onBlur={onFocusBlur}
-                    onFocus={onFocus}
-                    value={search}
-                    placeholder="Въведете населено място или адрес"
-                />
+        <section>
+            <article className={styles['header']}>
+                <img className={styles['header-img']} src="images/geo.jpg" alt="Geo" />
+                <h1 className={styles['header-title']}>Търсене на адреси</h1>
+            </article>
 
-                {search.length > 0 &&
-                    <button
-                        className={styles['search-container-close-btn']}
-                        onClick={onClose}>
-                        <span> &#10005;</span>
-                    </button>}
 
-                <button type="submit"
-                    className={styles['search-container-btn']} >
-                    <span className={styles['search-container-btn-search']}> &#9740;</span>
-                </button>
-            </div>
+            <article className={styles.content}>
+                <div className={styles['content-image-wrapper']}>
+                    <img className={styles['content-image']} src="images/bulgaria-map.jpg" alt="Map" />
+                </div>
+                <div className={styles['container']}>
+                    <form className={styles['container-form']}>
+                        <input
+                            type="text"
+                            className={styles['container-form-input']}
+                            name="search"
+                            onChange={onChange}
+                            onFocus={onFocus}
+                            value={search}
+                            placeholder="Въведете населено място или адрес"
+                        />
 
-            {selected.list.length > 0 && (
-                <ul className={styles['search-result']} style={selected.style}>
-                    {selected.list.map(x =>
-                    (<li
-                        key={x.magicKey}
-                        className={styles['search-result-item']}
-                        onClick={() => console.log(x.text)}
+                        {search.length > 0 &&
+                            <button
+                                className={styles['container-form-close-btn']}
+                                onClick={onClose}>
+                                <span> &#10005;</span>
+                            </button>}
 
-                    >
-                        {x.text}
-                    </li>))}
-                </ul>
-            )}
-        </form>
+                        <button type="submit"
+                            className={styles['container-form-btn']} >
+                            <span className={styles['container-form-btn-search']}> &#9740;</span>
+                        </button>
+                    </form>
+                    {selected.list.length > 0 && selected.display && (
+                        <ul className={styles['container-result']}>
+                            {selected.list.map(x =>
+                            (<li
+                                key={x.magicKey}
+                                className={styles['container-result-item']}
+                                onClick={onSelect}
+                            >
+                                {x.text}
+                            </li>))}
+                        </ul>
+                    )}
+                </div>
+            </article>
+        </section>
     );
 
 }
--- src/services/addressService.js
+++ src/services/addressService.js
@@ -1,6 +1,7 @@
 
 const endpoints = {
     suggest: (text) => `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&text=${text}`,
+    getAddress: (address) => `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=${encodeURIComponent(address)}%20BGR&f=json`
 }
 
 export async function getSuggested(text) {
@@ -17,11 +18,31 @@ export async function getSuggested(text) {
 
         const data = await response.json();
         return data.suggestions;
+
     } catch (err) {
         console.log(err);
     }
 
 }
 
+export async function getAddress(address) {
 
+    try {
+
+        const response = await fetch(endpoints.getAddress(address), {
+            method: 'GET'
+        })
+
+        if (response.ok === false) {
+            const error = await response.json();
+            throw new Error(error);
+        }
+        const result = await response.json();
+        return result.candidates;
+
+    } catch (err) {
+        console.log(err)
+    }
+
+}
 
