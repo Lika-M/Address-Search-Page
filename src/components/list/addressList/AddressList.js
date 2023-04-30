aa1d5603cca2a89856d7ccc751da83a53ab6a70f
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-04-30 16:06:01 +0300

    Code refactoring and adding simple footer.

--- src/components/list/addressList/AddressList.js
+++ src/components/list/addressList/AddressList.js
@@ -7,13 +7,18 @@ const AddressList = ({ list, isFound }) => {
         <section className={styles.list}>
             {list.length > 0 &&
                 <>
+                    <div className={styles.separator}></div>
                     <h2 className={styles['list-title']}>Намерени съвпадения:</h2>
                     <div className={styles.address}>
                         {list.map((x, i) => <AddressItem key={i} content={x} />)}
                     </div>
                 </>}
             {!list.length && isFound &&
-                <h2 className={styles['list-title']}>Не са открити съвпадения:</h2>}
+                <>
+                    <div className={styles.separator}></div>
+                    <h2 className={styles['list-title']}>Не са открити съвпадения:</h2>
+                    </>}
+           
         </section>
     );
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -132,9 +132,18 @@ const Search = () => {
                 </article>
             </section>
 
-            <div className={styles.separator}></div>
+          
 
             <AddressList list={result.data} isFound={result.isFound} />
+
+            <footer className={styles.footer}>
+                <>
+                    <div className={styles.separator}></div>
+                    <div className={styles['footer-text']}>
+                        &#10026; designed by Lika-M &copy; 2023 &#10026;
+                    </div>
+                </>
+            </footer>
 
         </div>
     );
